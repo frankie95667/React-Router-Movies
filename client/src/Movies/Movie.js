a95b969ecb4a0b6ef2b0430708a4f84f3f32cb14
Anthony Kawa <anthonyk2020@gmail.com>
2020-02-21 20:29:22 -0800

    completed MVP and stretch goals

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,26 +5,27 @@ const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
+          console.log(response);
           setMovie(response.data);
         })
         .catch(error => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -49,7 +50,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div onClick={()=> saveMovie()} className="save-button">Save</div>
     </div>
   );
 }
